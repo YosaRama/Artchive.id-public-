eb32be0623abaa6b8bfa73460850c3d9d6c2fc4d
Yosa Rama Dinata <yosamelody07@gmail.com>
2021-12-24 22:50:28 +0800

    ADD: Config for favicon

--- pages/_document.js
+++ pages/_document.js
@@ -6,12 +6,7 @@ class MyDocument extends Document {
       <Html lang="en-US">
         <Head>
           <meta name="description" content="" />
-          <link
-            rel="icon"
-            href="/images/favicon.svg"
-            type="image/svg"
-            sizes="16x16"
-          />
+          <link rel="icon" href="/images/favicon.svg" type="image/svg" sizes="16x16" />
         </Head>
         <body>
           <Main />
