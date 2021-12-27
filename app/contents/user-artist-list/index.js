fb0822ee1432678f0a17f4586761e9385e5e258e
Yosa Rama Dinata <yosamelody07@gmail.com>
2021-12-27 16:28:53 +0800

    ADD: Layout for user artist list page

--- app/contents/user-artist-list/index.js
+++ app/contents/user-artist-list/index.js
@@ -1,10 +1,66 @@
+// Libs
+import { useState } from "react";
+import { Button, Col, Row, Input, Drawer, Form } from "antd";
+const { Search } = Input;
+
 // Component
+import ContainerCard from "app/components/container/containerCard";
 import ContainerBox from "app/components/container/containerBox";
+import CardUserList from "app/components/libs/card-user-list";
+
+// Icons
+import { FilterOutlined, PlusOutlined } from "@ant-design/icons";
+import DashboardSearch from "app/components/libs/dashboard-search";
 
 function ArtistList() {
+  //? ============== Search Handling ============= ?//
+  const [searchValue, setSearchValue] = useState();
+  // * ====================================== * //
+
   return (
     <ContainerBox>
-      <h1>This is artist list</h1>
+      <ContainerCard
+        title="Artist List"
+        extra={
+          <>
+            <Row gutter={[16, 0]}>
+              {/* Filter Section */}
+              <Col>
+                <Button>
+                  <span>
+                    <FilterOutlined /> Filter
+                  </span>
+                </Button>
+              </Col>
+              {/* <Drawer visible={true}>Halo</Drawer> */}
+              {/* ===================================== */}
+
+              {/* Search Section */}
+              <Col>
+                <DashboardSearch
+                  searchValue={searchValue}
+                  setSearchValue={setSearchValue}
+                  searchBy="name"
+                />
+              </Col>
+              {/* ===================================== */}
+            </Row>
+          </>
+        }
+      >
+        <Col span={24} style={{ textAlign: "right", marginBottom: 20 }}>
+          <Button type="primary">
+            <span>
+              <PlusOutlined />
+            </span>
+            Add artist
+          </Button>
+        </Col>
+        <CardUserList />
+        <CardUserList />
+        <CardUserList />
+        <CardUserList />
+      </ContainerCard>
     </ContainerBox>
   );
 }
