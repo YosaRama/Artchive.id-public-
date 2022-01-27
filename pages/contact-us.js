10822175635bb71d950ed24a5b59243900bf07e7
Yosa Rama Dinata <yosamelody07@gmail.com>
2022-01-27 21:47:47 +0800

    ADD : Contact Us Layout

---
+++ pages/contact-us.js
@@ -0,0 +1,13 @@
+// Contents
+
+import ContactUsPage from "themes/contents/contact-us";
+
+function PageContactUs() {
+  return (
+    <>
+      <ContactUsPage />
+    </>
+  );
+}
+
+export default PageContactUs;
---
+++ themes/contents/contact-us/index.js
@@ -0,0 +1,46 @@
+// Libs
+import { Col, Form, Input } from "antd";
+
+// Components
+import PageContainerBox from "themes/components/container/box-container";
+import SimplePageContainer from "themes/components/container/simple-page-container";
+import PageButton from "themes/components/libs/page-button";
+
+// Styles
+import s from "./index.module.scss";
+
+function ContactUsPage() {
+  return (
+    <PageContainerBox>
+      <SimplePageContainer imgSrc="/images/contact-background.jpg" cardClassName="halo">
+        <section style={{ textAlign: "center" }} className={s.section}>
+          <Col span={24} className={s.title}>
+            <h1>Contact Us</h1>
+          </Col>
+
+          <section className={s.formSection}>
+            <Col span={24}>
+              <Form layout="vertical">
+                <Form.Item name="name">
+                  <Input placeholder="Name" />
+                </Form.Item>
+                <Form.Item name="email">
+                  <Input placeholder="Email Address" />
+                </Form.Item>
+                <Form.Item name="message">
+                  <Input.TextArea rows={12} placeholder="Message" />
+                </Form.Item>
+              </Form>
+            </Col>
+          </section>
+
+          <Col span={24}>
+            <PageButton type={"default " + s.button}>SEND</PageButton>
+          </Col>
+        </section>
+      </SimplePageContainer>
+    </PageContainerBox>
+  );
+}
+
+export default ContactUsPage;
