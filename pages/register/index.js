7465802c62876ff1223b521aa695efb1640a3b3d
yosaramadinata <yosamelody07@gmail.com>
2022-02-19 16:31:47 +0800

    ADD: Limit Access Registration & Login Page When User Already Login

--- pages/register/index.js
+++ pages/register/index.js
@@ -1,3 +1,6 @@
+// Libs
+import { getSession } from "next-auth/react";
+
 // Content
 import RegisterPage from "themes/contents/register";
 
@@ -10,3 +13,22 @@ function PageRegister() {
 }
 
 export default PageRegister;
+
+export const getServerSideProps = async (ctx) => {
+  const session = await getSession(ctx);
+
+  if (session) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: true,
+      },
+    };
+  } else {
+    return {
+      props: {
+        data: null,
+      },
+    };
+  }
+};
--- pages/signin.js
+++ pages/signin.js
@@ -1,3 +1,6 @@
+// Libs
+import { getSession } from "next-auth/react";
+
 // Contents
 import SignInPage from "themes/contents/signin";
 
@@ -6,3 +9,22 @@ function PageSignIn() {
 }
 
 export default PageSignIn;
+
+export const getServerSideProps = async (ctx) => {
+  const session = await getSession(ctx);
+
+  if (session) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: true,
+      },
+    };
+  } else {
+    return {
+      props: {
+        data: null,
+      },
+    };
+  }
+};
