086ae6473dc9c02b6ead0d81d2894ef32a4258d1
yosaramadinata <yosamelody07@gmail.com>
2022-02-26 01:17:26 +0800

    ADD: Artist List Responsive

--- themes/contents/artist/list/index.js
+++ themes/contents/artist/list/index.js
@@ -45,47 +45,55 @@ function ThemesContentsArtistList(props) {
         </ThemesBanner>
       </section>
 
-      <Affix>
-        <ThemesContainerMain sectionclass={s.searchSection}>
-          <section className={s.searchContainer}>
-            <Form>
-              <Row gutter={[16, 0]}>
-                <Col span={6}>
-                  <Form.Item name={"city"}>
-                    <Input placeholder="Search by City..." width={"100%"} />
-                  </Form.Item>
-                </Col>
-                <Col span={6}>
-                  <Form.Item name={"genre"}>
-                    <Input placeholder="Search by Genre..." width={"100%"} />
-                  </Form.Item>
-                </Col>
-                <Col span={6}>
-                  <Form.Item name={"name"}>
-                    <Input placeholder="Search by Name..." width={"100%"} />
-                  </Form.Item>
-                </Col>
-                <Col span={6}>
-                  <Row gutter={[16, 0]}>
-                    <Col span={12}>
-                      <ThemesButton type={`outlined ${s.button}`}>RESET</ThemesButton>
-                    </Col>
-                    <Col span={12}>
-                      <ThemesButton type={`default ${s.button}`}>SEARCH</ThemesButton>
-                    </Col>
-                  </Row>
-                </Col>
-              </Row>
-            </Form>
-          </section>
-        </ThemesContainerMain>
-      </Affix>
+      <div className={s.mobileHidden}>
+        <Affix>
+          <ThemesContainerMain sectionclass={s.searchSection}>
+            <section className={s.searchContainer}>
+              <Form>
+                <Row gutter={[16, 0]}>
+                  <Col span={6}>
+                    <Form.Item name={"city"}>
+                      <Input placeholder="Search by City..." width={"100%"} />
+                    </Form.Item>
+                  </Col>
+                  <Col span={6}>
+                    <Form.Item name={"genre"}>
+                      <Input placeholder="Search by Genre..." width={"100%"} />
+                    </Form.Item>
+                  </Col>
+                  <Col span={6}>
+                    <Form.Item name={"name"}>
+                      <Input placeholder="Search by Name..." width={"100%"} />
+                    </Form.Item>
+                  </Col>
+                  <Col span={6}>
+                    <Row gutter={[16, 0]}>
+                      <Col span={12}>
+                        <ThemesButton type={`outlined ${s.button}`}>RESET</ThemesButton>
+                      </Col>
+                      <Col span={12}>
+                        <ThemesButton type={`default ${s.button}`}>SEARCH</ThemesButton>
+                      </Col>
+                    </Row>
+                  </Col>
+                </Row>
+              </Form>
+            </section>
+          </ThemesContainerMain>
+        </Affix>
+      </div>
 
       <ThemesContainerMain>
         <section className={s.listSection}>
           <Row gutter={[16, 32]}>
             {(artistData ? artistData : initialArtistData)?.map((item, index) => (
-              <Col span={6} key={index}>
+              <Col
+                xl={{ span: 6 }}
+                lg={{ span: 8 }}
+                md={{ span: 12 }}
+                xs={{ span: 24 }}
+                key={index}
+              >
                 <ThemesArtistCard
                   artistId={item.id}
                   artistSlug={item.slug}
