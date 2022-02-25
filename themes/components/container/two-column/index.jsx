a6e4bd674820558a8149918144482b9e12a1dfc5
yosaramadinata <yosamelody07@gmail.com>
2022-02-26 02:29:59 +0800

    ADD: Contact Us Responsive

--- themes/components/container/two-column/index.jsx
+++ themes/components/container/two-column/index.jsx
@@ -3,12 +3,15 @@ import propTypes from "prop-types";
 import Image from "next/image";
 import { Card, Col, Row } from "antd";
 
+// Styles
+import s from "./index.module.scss";
+
 function ThemesContainerTwoColumns(props) {
   const { imgSrc, children } = props;
   return (
     <section>
       <Row>
-        <Col span={12}>
+        <Col lg={{ span: 12 }} xs={{ span: 24 }} className={s.bannerContainer}>
           <Image
             src={imgSrc ? imgSrc : "/images/artwork-1.jpg"}
             layout="fill"
@@ -16,7 +19,7 @@ function ThemesContainerTwoColumns(props) {
             objectFit="cover"
           />
         </Col>
-        <Col span={12} style={{ padding: "50px", minHeight: "70vh", paddingRight: "0" }}>
+        <Col lg={{ span: 12 }} xs={{ span: 24 }} className={s.contentContainer}>
           <Card className="simplePageContainer-card">{children}</Card>
         </Col>
       </Row>
