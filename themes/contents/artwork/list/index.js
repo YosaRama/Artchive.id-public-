b4a67f792c9fd2718409ef315b164f506d602c01
yosaramadinata <yosamelody07@gmail.com>
2022-03-05 23:47:13 +0800

    FIX: Search on Artwork List

--- themes/contents/artwork/list/index.js
+++ themes/contents/artwork/list/index.js
@@ -18,6 +18,7 @@ import { useWindowSize } from "app/helpers/useWindowSize";
 
 // Styles
 import s from "./index.module.scss";
+import Sticky from "react-sticky-el";
 
 function ThemesContentsArtworkList() {
   //? ============== Handle Get Viewport ============= ?//
@@ -57,59 +58,68 @@ function ThemesContentsArtworkList() {
         </ThemesBanner>
       </section>
 
-      <section>
-        <ThemesContainerMain>
-          <section style={{ margin: "50px 0" }}>
-            <Row justify="space-between">
-              <Col span={6} className={s.mobileHidden} style={{ height: "fit-content" }}>
-                <Affix offsetTop={80}>
-                  <Card>
-                    <Col span={24} className={s.searchTitle}>
-                      <h1>Search By</h1>
-                    </Col>
-                    <Col span={24}>
-                      <Form>
-                        <Form.Item name="artist_name">
-                          <Select showSearch placeholder="Artist Name">
-                            <Option value={["john doe", 1]}>John Doe</Option>
-                            <Option value={["mark sue", 2]}>Mark Sue</Option>
-                            <Option value={["marcus hitler", 3]}>Marcus Hitler</Option>
-                          </Select>
-                        </Form.Item>
-                        <Form.Item name="artwork_title">
-                          <Input placeholder="Artwork Title" />
-                        </Form.Item>
-                        <Form.Item>
-                          <Select showSearch placeholder="Genre">
-                            <Option value={["abstract", 1]}>Abstract</Option>
-                            <Option value={["cubism", 2]}>Cubism</Option>
-                            <Option value={["realism", 3]}>Realism</Option>
-                          </Select>
-                        </Form.Item>
-                        <Col span={24} className={s.priceTitle}>
-                          <p>Price</p>
-                        </Col>
-                        <Row justify="space-between">
-                          <Col className={s.priceTag}>IDR {minPrice}</Col>
-                          <Col className={s.priceTag}>IDR {maxPrice}</Col>
-                        </Row>
-                        <Form.Item>
-                          <Slider
-                            range
-                            defaultValue={[minPrice, maxPrice]}
-                            max={100000000}
-                            min={1000000}
-                            step={1000000}
-                            onChange={handlePriceChange}
-                          />
-                        </Form.Item>
-                      </Form>
-                      <Col span={24}>
-                        <ThemesButton type={"default " + s.searchButton}>SEARCH</ThemesButton>
+      <section className="">
+        <ThemesContainerMain containerClass="">
+          <section style={{ margin: "50px 0" }} className="">
+            <Row justify="space-between" className="boundary">
+              <Col span={6} className={`${s.mobileHidden} `} style={{ height: "auto" }}>
+                {/* <Affix offsetTop={80}> */}
+                <div className="affixContainer">
+                  <Sticky
+                    boundaryElement=".boundary"
+                    hideOnBoundaryHit={false}
+                    stickyStyle={{ paddingTop: 80 }}
+                  >
+                    <Card>
+                      <Col span={24} className={s.searchTitle}>
+                        <h1>Search By</h1>
                       </Col>
-                    </Col>
-                  </Card>
-                </Affix>
+                      <Col span={24}>
+                        <Form>
+                          <Form.Item name="artist_name">
+                            <Select showSearch placeholder="Artist Name">
+                              <Option value={["john doe", 1]}>John Doe</Option>
+                              <Option value={["mark sue", 2]}>Mark Sue</Option>
+                              <Option value={["marcus hitler", 3]}>Marcus Hitler</Option>
+                            </Select>
+                          </Form.Item>
+                          <Form.Item name="artwork_title">
+                            <Input placeholder="Artwork Title" />
+                          </Form.Item>
+                          <Form.Item>
+                            <Select showSearch placeholder="Genre">
+                              <Option value={["abstract", 1]}>Abstract</Option>
+                              <Option value={["cubism", 2]}>Cubism</Option>
+                              <Option value={["realism", 3]}>Realism</Option>
+                            </Select>
+                          </Form.Item>
+                          <Col span={24} className={s.priceTitle}>
+                            <p>Price</p>
+                          </Col>
+                          <Row justify="space-between">
+                            <Col className={s.priceTag}>IDR {minPrice}</Col>
+                            <Col className={s.priceTag}>IDR {maxPrice}</Col>
+                          </Row>
+                          <Form.Item>
+                            <Slider
+                              range
+                              defaultValue={[minPrice, maxPrice]}
+                              max={100000000}
+                              min={1000000}
+                              step={1000000}
+                              onChange={handlePriceChange}
+                            />
+                          </Form.Item>
+                        </Form>
+                        <Col span={24}>
+                          <ThemesButton type={"default " + s.searchButton}>SEARCH</ThemesButton>
+                        </Col>
+                      </Col>
+                    </Card>
+                  </Sticky>
+                </div>
+
+                {/* </Affix> */}
               </Col>
 
               <Col xl={{ span: 17 }} lg={{ span: 24 }}>
