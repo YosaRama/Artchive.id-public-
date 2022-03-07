fc69b14d485ef850bf0c5f766b85db39b8578fe7
yosaramadinata <yosamelody07@gmail.com>
2022-03-08 00:32:14 +0800

    UPDATE: Profile Navbar Styling

--- themes/components/container/profile/index.js
+++ themes/components/container/profile/index.js
@@ -1,5 +1,8 @@
 // Libs
 import { Row, Col } from "antd";
+import Sticky from "react-sticky-el";
+
+// Components
 import ThemesProfileNavbar from "themes/components/libs/profile-navbar";
 import ThemesContainerMain from "../main";
 
@@ -11,8 +14,10 @@ function ThemesContainerProfile(props) {
   return (
     <>
       <Row className={s.container}>
-        <Col span={1} className={`${s.mobileHidden} ${s.navbar}`}>
-          <ThemesProfileNavbar />
+        <Col span={1} className={`${s.mobileHidden} ${s.navbarWrapper} navbarBoundary`}>
+          <Sticky boundaryElement=".navbarBoundary" stickyStyle={{ paddingTop: 64 }}>
+            <ThemesProfileNavbar />
+          </Sticky>
         </Col>
         <Col xl={{ span: 23 }} xs={{ span: 24 }} className={`${s.contentWrapper}`}>
           <ThemesContainerMain sectionclass={s.contentSection}>{children}</ThemesContainerMain>
