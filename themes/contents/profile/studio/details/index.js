dc6bc46bd280ca3564ad0a3dfc3bf6b3a792764a
yosaramadinata <yosamelody07@gmail.com>
2022-03-14 22:30:26 +0800

    ADD: Profile Studio Artwork Edit Page Layout

--- themes/contents/profile/studio/details/index.js
+++ themes/contents/profile/studio/details/index.js
@@ -1,7 +1,301 @@
-function ThemesContentsProfileStudioDetails() {
+// Libs
+import { useRouter } from "next/router";
+import { useState } from "react";
+import { Col, Divider, Row, Button, Form, Input, Image, Select, InputNumber } from "antd";
+const { Option } = Select;
+
+// Data Hook
+import { useUploads } from "app/hooks/upload";
+import { useArtwork } from "app/hooks/artwork";
+import { useGenres } from "app/hooks/genre";
+
+// Components
+import ThemesHeadline from "themes/components/libs/headline";
+import ThemesButton from "themes/components/libs/button";
+import AppUploadBox from "app/components/libs/upload-box";
+import deleteConfirmModal from "app/components/utils/delete-modal-confirm";
+import { WarningNotification } from "app/components/utils/notification";
+
+// Helpers
+import priceFormatter from "app/helpers/priceFormatter";
+
+// Styles
+import s from "./index.module.scss";
+
+function ThemesContentsProfileStudioDetails(props) {
+  const { artistData } = props;
+  const router = useRouter();
+  const artworkId = router.query.id;
+  const [form] = Form.useForm();
+
+  //? ============== Artwork Hook ============= ?//
+  const { data: artworkData } = useArtwork({ singleId: artworkId });
+  // * ====================================== * //
+
+  //? ============== Genre Hook ============= ?//
+  const { data: genreData } = useGenres({ queryString: "" });
+  // * ====================================== * //
+
+  //? ============== Handle Initial Data ============= ?//
+  const initialData = artworkData && {
+    ...artworkData,
+    genre: artworkData.genre.map((item) => item.id),
+  };
+
+  // * ====================================== * //
+
+  //? ============== Handle Submit ============= ?//
+  const handleSubmit = () => {
+    form.validateFields().then(async (value) => {
+      const submission = {
+        artist_id: artistData?.id,
+        title: value.title,
+        year: value.year,
+        material: value.material,
+        description: value.description,
+        genre_id: value.genre,
+        media_id: [],
+        cover_id: uploadImage?.id,
+        type: value.type,
+        height: value.height,
+        width: value.width,
+        price: `${value.price}`,
+        status: "DRAFT",
+        approve: false,
+      };
+      console.log(submission);
+      // if (!submission.cover_id) {
+      //   WarningNotification({
+      //     message: "Failed Submit!",
+      //     description: "Please upload artwork image",
+      //   });
+      // } else {
+      //   const result = await onAdd(submission);
+      // }
+    });
+  };
+  // * ====================================== * //
+
+  //? ============== Handle Upload ============= ?//
+  const { loading: uploadLoading, onUpload } = useUploads();
+  const [uploadImage, setUploadImage] = useState(true);
+  const handleUpload = async (file) => {
+    const result = await onUpload({
+      file: file.file,
+      userId: artistData.id,
+      artworkId: lastArtworkId,
+    });
+    if (result.success) {
+      setUploadImage({ id: result.data.id, url: result.data.url });
+    }
+  };
+  // * ====================================== * //
+
   return (
     <>
-      <></>
+      {artworkData && (
+        <>
+          {/* Form Details Section */}
+          <div className={s.title}>
+            <ThemesHeadline title="Edit Your Artwork" subtitle="Your artwork details" />
+          </div>
+          <Divider className={s.divider} />
+          <div className={s.contentContainer}>
+            <Row gutter={[32, 0]} justify="center">
+              <Col lg={{ span: 10 }} xs={{ span: 24 }} className={s.artworkImage}>
+                {uploadImage ? (
+                  <>
+                    <Image
+                      alt=""
+                      src={`${process.env.NEXT_PUBLIC_S3_URL}/${
+                        uploadImage?.url || artworkData?.media_cover?.url
+                      }`}
+                    />
+                    <Col span={24} className={s.changeImgBtn} style={{ padding: 0 }}>
+                      <Button onClick={() => setUploadImage()}>Remove Image</Button>
+                    </Col>
+                  </>
+                ) : (
+                  <AppUploadBox
+                    onUpload={handleUpload}
+                    loading={uploadLoading}
+                    className={s.upload}
+                  />
+                )}
+              </Col>
+              <Col lg={{ span: 12 }} xs={{ span: 24 }}>
+                <Form layout="vertical" form={form} initialValues={initialData}>
+                  <Form.Item
+                    name="genre"
+                    label="Genre"
+                    rules={[{ required: true, message: "Please select genre for this artwork!" }]}
+                  >
+                    <Select placeholder="Select artist" showSearch mode="multiple">
+                      {genreData &&
+                        genreData?.map((item, index) => {
+                          return (
+                            <Option key={index} value={item.id}>
+                              {item.title}
+                            </Option>
+                          );
+                        })}
+                    </Select>
+                  </Form.Item>
+                  <Form.Item
+                    name="title"
+                    label="Title"
+                    rules={[{ required: true, message: "Please input title for this artwork!" }]}
+                  >
+                    <Input placeholder="Input artwork title" />
+                  </Form.Item>
+                  <Form.Item
+                    name="year"
+                    label="Year"
+                    rules={[{ required: true, message: "Please input year for this artwork!" }]}
+                  >
+                    <Input placeholder="Input artwork year" />
+                  </Form.Item>
+                  <Form.Item
+                    name="material"
+                    label="Material"
+                    rules={[{ required: true, message: "Please input material for this artwork!" }]}
+                  >
+                    <Input placeholder="Input artwork material" />
+                  </Form.Item>
+                  <Form.Item
+                    name="description"
+                    label="Description"
+                    rules={[
+                      { required: true, message: "Please input description for this artwork!" },
+                    ]}
+                  >
+                    <Input.TextArea placeholder="Write artwork description" />
+                  </Form.Item>
+
+                  <Form.Item
+                    name="type"
+                    label="Type"
+                    rules={[{ required: true, message: "Please select type of this artwork!" }]}
+                  >
+                    <Select placeholder="Select type of artwork">
+                      <Option value="UNIQUE">Unique</Option>
+                      <Option value="EDITION">Edition Series</Option>
+                    </Select>
+                  </Form.Item>
+                  <Row gutter={[16, 0]}>
+                    <Col span={12}>
+                      <Form.Item
+                        name="width"
+                        label="Width"
+                        rules={[
+                          {
+                            required: true,
+                            message: "Please input width for this artwork!",
+                            type: "number",
+                          },
+                        ]}
+                      >
+                        <InputNumber placeholder="Width (cm)" style={{ width: "100%" }} />
+                      </Form.Item>
+                    </Col>
+                    <Col span={12}>
+                      <Form.Item
+                        name="height"
+                        label="Height"
+                        rules={[
+                          {
+                            required: true,
+                            message: "Please input height for this artwork!",
+                            type: "number",
+                          },
+                        ]}
+                      >
+                        <InputNumber placeholder="Height (cm)" style={{ width: "100%" }} />
+                      </Form.Item>
+                    </Col>
+                  </Row>
+                  <Form.Item
+                    name="price"
+                    label="Price"
+                    rules={[
+                      {
+                        required: true,
+                        message: "Please input price for this artwork!",
+                      },
+                    ]}
+                  >
+                    <InputNumber
+                      style={{ width: "100%" }}
+                      placeholder="Input artwork price"
+                      addonBefore="Rp"
+                      formatter={(value) => priceFormatter(`Rp ${value}`, ",")}
+                      parser={(value) => value.replace(/Rp\s?|(,*)/g, "")}
+                    />
+                  </Form.Item>
+                </Form>
+              </Col>
+            </Row>
+          </div>
+          {/* =================================== */}
+
+          {/* Media Galley Section */}
+          <div className={s.title}>
+            <ThemesHeadline title="Media Gallery" subtitle="Upload your artwork details photos" />
+          </div>
+          <Divider className={s.divider} />
+          <div className={s.contentContainer}>
+            <Row gutter={[16, 0]} justify="center">
+              <Col span={22}>
+                {artworkData?.media_gallery?.map((item) => {
+                  return (
+                    <Col className={s.image} span={6} style={{ textAlign: "center" }} key={item.id}>
+                      <Image src={`${process.env.NEXT_PUBLIC_S3_URL}/${item.url}`} alt="" />
+                      <Button
+                        onClick={() =>
+                          deleteConfirmModal({
+                            title: "artwork gallery image",
+                            onDelete: () => handleDelete(item.id),
+                          })
+                        }
+                      >
+                        Delete
+                      </Button>
+                    </Col>
+                  );
+                })}
+
+                {artworkData?.media_gallery?.length < 4 && (
+                  <Col span={6}>
+                    <AppUploadBox
+                      className={s.uploadBox}
+                      onUpload={handleUpload}
+                      loading={uploadLoading}
+                    />
+                  </Col>
+                )}
+              </Col>
+            </Row>
+          </div>
+          {/* =================================== */}
+
+          {/* Action Button Section */}
+          <div>
+            <Col span={8} className={s.submitBtnContainer}>
+              <Row gutter={[16, 0]} justify="end">
+                <Col span={12}>
+                  <ThemesButton type={`outline ${s.submitBtn}`}>DELETE</ThemesButton>
+                </Col>
+                <Col span={12}>
+                  <ThemesButton type={`primary ${s.submitBtn}`} onClick={handleSubmit}>
+                    SAVE
+                  </ThemesButton>
+                </Col>
+              </Row>
+            </Col>
+          </div>
+          {/* =================================== */}
+        </>
+      )}
     </>
   );
 }
