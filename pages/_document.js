127c4f78c983d3cb30a14e259295ecca1a128c23
yosaramadinata <yosamelody07@gmail.com>
2022-04-01 00:45:25 +0800

    UPDATE: GTM Code

--- pages/_document.js
+++ pages/_document.js
@@ -9,10 +9,10 @@ class MyDocument extends Document {
         dangerouslySetInnerHTML={{
           __html: `
           (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-PM2HWVC');
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-T99SD58');
         `,
         }}
       />
@@ -22,8 +22,8 @@ class MyDocument extends Document {
     return (
       <noscript
         dangerouslySetInnerHTML={{
-          __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PM2HWVC"
-      height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+          __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-T99SD58"
+height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
         }}
       />
     );
