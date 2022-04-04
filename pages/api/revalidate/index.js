e5105f4982984fc8397350be2b82288c02c3f4e6
yosaramadinata <yosamelody07@gmail.com>
2022-04-04 23:08:09 +0800

    FIX: Update Artwork

--- pages/api/revalidate/index.js
+++ pages/api/revalidate/index.js
@@ -6,7 +6,6 @@ export default async function handler(req, res) {
     await res.unstable_revalidate(path);
     return res.json({ revalidated: true });
   } catch (err) {
-    console.log(err);
     return res.status(500).send("Error revalidating");
   }
 }
