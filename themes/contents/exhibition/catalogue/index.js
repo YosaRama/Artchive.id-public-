8799feb314f38ab6f59ba37e9146c1df06a30c1f
Yosa Rama <yosamelody07@gmail.com>
2022-05-08 20:07:34 +0800

    ADD: Exhibition Catalogue Page

--- themes/contents/exhibition/catalogue/index.js
+++ themes/contents/exhibition/catalogue/index.js
@@ -1,7 +1,68 @@
+// Libs
+import FlipBook from "react-pageflip";
+
+// Components
+import ThemesContainerMain from "themes/components/container/main";
+
+// Styles
+import s from "./index.module.scss";
+
 function ThemesContentsExhibitionCatalogue() {
   return (
     <>
-      <></>
+      <ThemesContainerMain>
+        <section className={s.section}>
+          <FlipBook
+            width={550}
+            height={733}
+            size="stretch"
+            minWidth={315}
+            maxWidth={1000}
+            minHeight={400}
+            maxHeight={1533}
+            maxShadowOpacity={0.5}
+            showCover={true}
+            mobileScrollSupport={true}
+            className="demo-book"
+          >
+            <div>
+              <div style={{ height: "100%", width: "100%" }}>
+                <img src="/images/artwork-1.jpg" style={{ width: "100%", height: "100%" }} />
+              </div>
+            </div>
+            <div>
+              <div style={{ height: "100%", width: "100%" }}>
+                <img src="/images/artwork-2.jpg" style={{ width: "100%", height: "100%" }} />
+              </div>
+            </div>
+            <div>
+              <div style={{ height: "100%", width: "100%" }}>
+                <img src="/images/artwork-3.jpg" style={{ width: "100%", height: "100%" }} />
+              </div>
+            </div>
+            <div>
+              <div style={{ height: "100%", width: "100%" }}>
+                <img src="/images/artwork-4.jpg" style={{ width: "100%", height: "100%" }} />
+              </div>
+            </div>
+            <div>
+              <div style={{ height: "100%", width: "100%" }}>
+                <img src="/images/artwork-5.jpg" style={{ width: "100%", height: "100%" }} />
+              </div>
+            </div>
+            <div>
+              <div style={{ height: "100%", width: "100%" }}>
+                <img src="/images/artwork-1.jpg" style={{ width: "100%", height: "100%" }} />
+              </div>
+            </div>
+            <div>
+              <div style={{ height: "100%", width: "100%" }}>
+                <img src="/images/artwork-2.jpg" style={{ width: "100%", height: "100%" }} />
+              </div>
+            </div>
+          </FlipBook>
+        </section>
+      </ThemesContainerMain>
     </>
   );
 }
