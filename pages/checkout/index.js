f06f5365ca63e4ca0c3a19fb6bc8f068c547d294
Yosa Rama <yosamelody07@gmail.com>
2022-09-07 01:23:48 +0800

    ADD: Redirect system on Checkout page

--- pages/checkout/index.js
+++ pages/checkout/index.js
@@ -1,3 +1,5 @@
+import { GET_CART_BY_USER_ID } from "app/database/query/cart";
+import { getSession } from "next-auth/react";
 import ThemesContentsCheckout from "themes/contents/checkout";
 
 function PageCheckout() {
@@ -9,3 +11,35 @@ function PageCheckout() {
 }
 
 export default PageCheckout;
+
+export const getServerSideProps = async (ctx) => {
+  const session = await getSession(ctx);
+
+  if (!session) {
+    return {
+      notFound: true,
+    };
+  }
+
+  //? ============== Get User Cart ============= ?//
+  const cartItem = await GET_CART_BY_USER_ID({ userId: session.user.id });
+  // * ====================================== * //
+
+  if (cartItem.length == 0) {
+    return {
+      props: {
+        data: null,
+      },
+      redirect: {
+        destination: "/cart",
+        permanent: true,
+      },
+    };
+  } else {
+    return {
+      props: {
+        data: null,
+      },
+    };
+  }
+};
