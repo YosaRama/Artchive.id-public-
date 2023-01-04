e989bc396123d2e83a6b6208996b805bf10c38d6
JOO DAN TAE <dionpr84@gmail.com>
2023-01-04 22:51:15 +0700

    UPDATE: Enabe Markup Price Value

---
+++ app/database/dummy/genre.js
@@ -0,0 +1,20 @@
+export const genreData = [
+  {
+    title: "Cubism",
+    subtitle: "Show Artwork | Sell Artwork | Manage Profile",
+    description: `An artist is a person engaged in an activity related to creating art, practicing the arts, or demonstrating an art. The common usage in both everyday speech and academic discourse refers to a practitioner in the visual arts only.`,
+    artwork: "/images/homepage-role-artist.jpg",
+  },
+  {
+    title: "Realistic",
+    subtitle: "Buy Artwork | Show Collection | Trade Collection",
+    description: `Art connoiseur. Art dealer. Art enthusiast. Art lover. “A famous art collector is walking through the city when he notices a mangy cat lapping milk from a saucer in the doorway of a store, and he does a double take”.`,
+    artwork: "/images/homepage-role-collector.jpg",
+  },
+  {
+    title: "Naive",
+    subtitle: `Show Gallery Artwork | Do Artwork Transaction`,
+    description: `An art gallery is an exhibition space to display and sell artworks. As a result, the art gallery is a commercial enterprise working with a portfolio of artists.`,
+    artwork: "/images/homepage-role-gallery.jpg",
+  },
+];
