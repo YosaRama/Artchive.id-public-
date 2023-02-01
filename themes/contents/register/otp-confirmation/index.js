45f94692b233743085ece8454c4370a83a688e45
JOO DAN TAE <dionpr84@gmail.com>
2023-02-01 20:33:17 +0700

    UPDATE: Otp Page Layout and Responsive

--- themes/contents/register/otp-confirmation/index.js
+++ themes/contents/register/otp-confirmation/index.js
@@ -1,5 +1,5 @@
 // Libs
-import { Col, Row, Image } from "antd";
+import { Col, Row, Image, Input, Form } from "antd";
 
 //Components
 import ThemesContainerMain from "themes/components/container/main";
@@ -20,7 +20,60 @@ function ThemesContentOtpConfirmation() {
           title="Phone Number Verification"
           subtitle="You will get OTP number verification on +62 85 858 396 893"
         />
-        <Col className={s.otp}></Col>
+
+        <Form>
+          <Form.Item>
+            <Input.Group>
+              <Row className={s.otp}>
+                <Col span={6} style={{ fontSize: "32px" }}>
+                  <h1>ART -</h1>
+                </Col>
+
+                <Col span={3}>
+                  <Input
+                    placeholder="0"
+                    bordered={true}
+                    maxLength={1}
+                    size="large"
+                    className={s.placeholder}
+                    autoSize={true}
+                  />
+                </Col>
+                <Col span={3}>
+                  <Input
+                    placeholder="0"
+                    bordered={true}
+                    maxLength={1}
+                    size="large"
+                    className={s.placeholder}
+                    autoSize={true}
+                  />
+                </Col>
+                <Col span={3}>
+                  <Input
+                    placeholder="0"
+                    bordered={true}
+                    maxLength={1}
+                    size="large"
+                    className={s.placeholder}
+                    autoSize={true}
+                  />
+                </Col>
+                <Col span={3}>
+                  <Input
+                    placeholder="0"
+                    bordered={true}
+                    maxLength={1}
+                    size="large"
+                    className={s.placeholder}
+                    autoSize={true}
+                  />
+                </Col>
+              </Row>
+            </Input.Group>
+          </Form.Item>
+        </Form>
+
         <ThemesButton type={"primary " + s.btn}>VERIFY</ThemesButton>
         <p>
           Not receiving any OTP notification? <span className={s.resend}>Resend again</span>.
