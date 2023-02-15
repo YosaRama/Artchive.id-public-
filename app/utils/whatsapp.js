2fdb91941b2b44b10c3ad69c4bb64d1f35ab2cf7
Yosa Rama <yosamelody07@gmail.com>
2023-02-15 22:13:07 +0800

    UPDATE: Whatsapp send otp message helper

--- app/utils/whatsapp.js
+++ app/utils/whatsapp.js
@@ -11,7 +11,7 @@ export async function sendOtpMessage({ phoneNumber, fullName, otpCode }) {
       },
       data: {
         messaging_product: "whatsapp",
-        to: "6282247352553",
+        to: phoneNumber,
         type: "template",
         template: {
           name: "otpmessage",
@@ -20,26 +20,15 @@ export async function sendOtpMessage({ phoneNumber, fullName, otpCode }) {
           },
           components: [
             {
-              type: "header",
-              parameters: [
-                {
-                  type: "image",
-                  image: {
-                    link: "https://images.unsplash.com/reserve/bOvf94dPRxWu0u3QsPjF_tree.jpg?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1176&q=80",
-                  },
-                },
-              ],
-            },
-            {
               type: "body",
               parameters: [
                 {
                   type: "text",
-                  text: "Yosa Rama",
+                  text: fullName,
                 },
                 {
                   type: "text",
-                  text: "ART-123-123",
+                  text: otpCode,
                 },
               ],
             },
