64aea14aa6f068c322b1976db65b68ff8bfcde44
JOO DAN TAE <dionpr84@gmail.com>
2023-08-18 14:37:11 +0700

    FIX: Minor Auction Banner for User Experience

--- themes/components/libs/banner-auction/index.jsx
+++ themes/components/libs/banner-auction/index.jsx
@@ -2,13 +2,21 @@
 import { Col, Row } from "antd";
 import propTypes from "prop-types";
 import moment from "moment";
-// import moment from "moment-timezone";
+
+// Helper
+import { useSession } from "next-auth/react";
+import { useWindowSize } from "app/helpers/useWindowSize";
+import nameAbbreviation from "app/helpers/nameAbbreviation";
 
 // Style
 import s from "./index.module.scss";
 
 function ThemesBannerAuctionItem(props) {
   const { title, startDate, endDate, placeName, loading } = props;
+  const { data: session } = useSession();
+  const userName = session?.user?.full_name;
+  const { width } = useWindowSize();
+
   //? ============== Countdown Timer ============= ?//
   const targetMoment = moment(endDate);
   const currentMoment = moment();
@@ -20,7 +28,11 @@ function ThemesBannerAuctionItem(props) {
       <Row className={s.bannerItem}>
         <Col span={24} className={s.description}>
           <h2>{title}</h2>
-          <p style={{ marginBottom: "15px" }}>
+          {session && session.user.role === "auction-participant" && (
+            <h3>Welcome, {width <= 500 ? nameAbbreviation(userName, 14) : userName}</h3>
+          )}
+
+          <p style={{ marginBottom: "0px" }}>
             Started on {moment(startDate).format("dddd, DD MMMM YYYY")}
             {placeName && `| {placeName}`}
           </p>
