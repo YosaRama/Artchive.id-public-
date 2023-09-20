20144cf46d83aabcb18ff19923643b8d370de746
JOO DAN TAE <dionpr84@gmail.com>
2023-09-21 03:23:20 +0700

    CREATE: 404 Not Found Page

---
+++ pages/404/index.js
@@ -0,0 +1,12 @@
+// Contents
+import ThemesContents404 from "themes/contents/404";
+
+function Page404() {
+  return (
+    <>
+      <ThemesContents404 />
+    </>
+  );
+}
+
+export default Page404;
